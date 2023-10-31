ec588d46c6c3002477ab3e1aafef0081cbf02d95
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-10-31 23:51:37 +0300

    Revert "Replaced database entities"
    This reverts commit 1cf222bc0f7a91144c83f67fb58d601e12731521.

---
+++ src/Shop.Persistance/EntityConfigurations/ShoesSizeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Persistence.EntityConfigurations
+{
+    internal class ShoesSizeConfiguration : IEntityTypeConfiguration<ShoesSize>
+    {
+        public void Configure(EntityTypeBuilder<ShoesSize> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Size)
+                   .IsRequired()
+                   .HasColumnOrder(1);
+            builder.Property(x => x.Price)
+                   .IsRequired()
+                   .HasColumnOrder(2);
+            builder.Property(x => x.ItemsLeft)
+                   .HasColumnOrder(3);
+        }
+    }
+}
