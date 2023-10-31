832e6110f0c7fa39c98751a81302f4eb18a148c6
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-10-31 23:52:11 +0300

    Revert "Revert "Replaced database entities""
    This reverts commit ec588d46c6c3002477ab3e1aafef0081cbf02d95.

---
+++ src/Shop.Persistance/EntityConfigurations/CartItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Persistence.EntityConfigurations
+{
+    internal class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Amount)
+                   .IsRequired();
+            builder.HasOne(x => x.ShopCart)
+                   .WithMany(x => x.Items)
+                   .HasForeignKey(x => x.ShopCartId);
+            builder.HasOne(x => x.ModelVariant)
+                   .WithMany(x => x.CartsIn)
+                   .HasForeignKey(x => x.ModeVariantId);
+        }
+    }
+}
---
+++ src/Shop.Persistance/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Persistence.EntityConfigurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x=>x.Id);
+            builder.Property(x => x.UserName)
+                   .HasMaxLength(128)
+                   .IsRequired();
+            builder.Property(x => x.Login)
+                   .HasMaxLength(128)
+                   .IsRequired();
+            builder.Property(x => x.Password)
+                   .HasMaxLength(256)
+                   .IsRequired();
+            builder.Property(x => x.Phone)
+                   .HasMaxLength(50);
+            builder.HasOne(x => x.Adress)
+                   .WithMany(x => x.Users)
+                   .HasForeignKey(x =>x.AdressId);
+        }
+    }
+}
