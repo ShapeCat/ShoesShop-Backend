1cf222bc0f7a91144c83f67fb58d601e12731521
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-10-31 04:15:38 +0300

    Replaced database entities

---
+++ src/Shop.Persistance/EntityConfigurations/ReviewConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Persistence.EntityConfigurations
+{
+    internal class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Rating)
+                   .IsRequired();
+            builder.Property(x => x.Comment);
+            builder.Property(x => x.PublishDate)
+                   .IsRequired();
+            builder.HasOne(x => x.Model)
+                   .WithMany(x => x.Rewiews)
+                   .HasForeignKey(x=>x.ModelId);
+            builder.HasOne(x => x.Author)
+                   .WithMany(x => x.Rewiews)
+                   .HasForeignKey(x => x.UserId);
+        }
+    }
+}
--- src/Shop.Persistance/ShopDbContext.cs
+++ src/Shop.Persistance/ShopDbContext.cs
@@ -6,17 +6,38 @@ namespace ShoesShop.Persistence
 {
     public class ShopDbContext : DbContext
     {
-        public DbSet<Shoes> Shoes { get; set; } = null!;
-        public DbSet<ShoesSize> Sizes { get; set; } = null!;
-        public DbSet<Description> Descriptions { get; set; } = null!;
+        public DbSet<Adress> Adresses { get; set; } = null!;
+        public DbSet<CartItem> CartItems { get; set; } = null!;
+        public DbSet<FavoritesItem> FavoritesItems { get; set; } = null!;
+        public DbSet<FavoritesList> FavoritesLists { get; set; } = null!;
+        public DbSet<Image> Images { get; set; } = null!;
+        public DbSet<Model> Models { get; set; } = null!;
+        public DbSet<ModelSize> ModelsSizes { get; set; } = null!;
+        public DbSet<ModelVariant> ModelsVariants { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderItem> OrderItems { get; set; } = null!;
+        public DbSet<Price> Prices { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+        public DbSet<ShopCart> ShopCarts { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
 
         public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new ShoesConfiguration());
-            modelBuilder.ApplyConfiguration(new ShoesSizeConfiguration());
-            modelBuilder.ApplyConfiguration(new DescriptionConfiguration());
+            modelBuilder.ApplyConfiguration(new AdressConfiguration());
+            modelBuilder.ApplyConfiguration(new CartItemConfiguration());
+            modelBuilder.ApplyConfiguration(new FavoriteItemConfiguration());
+            modelBuilder.ApplyConfiguration(new FavoritesListConfiguration());
+            modelBuilder.ApplyConfiguration(new ImageConfiguration());
+            modelBuilder.ApplyConfiguration(new ModelConfiguration());
+            modelBuilder.ApplyConfiguration(new ModelSizeConfiguration());
+            modelBuilder.ApplyConfiguration(new ModelVariantConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+            modelBuilder.ApplyConfiguration(new PriceConfiguration());
+            modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+            modelBuilder.ApplyConfiguration(new ShopCartConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
