38ab5239853bff1ad123622cd0cb3f473941c51e
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-05 08:08:07 +0300

    Added missing repository

---
+++ src/Shop.Persistance/Repository/FavoritesItemRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using ShoesShop.Application.Exceptions;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Persistence.Repository
+{
+    public class FavoritesItemRepository : GenericRepository<FavoritesItem>
+    {
+        public FavoritesItemRepository(ShopDbContext dbContext) : base(dbContext) { }
+
+        public override async Task<IEnumerable<FavoritesItem>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await dbSet.ToListAsync(cancellationToken);
+        }
+
+        public override async Task<FavoritesItem> GetAsync(Guid Id, CancellationToken cancellationToken)
+        {
+            return await dbSet.FirstOrDefaultAsync(cancellationToken)
+                   ?? throw new NotFoundException(Id.ToString(), typeof(FavoritesItem));
+        }
+
+        public override async Task<IEnumerable<FavoritesItem>> FindAllAsync(Expression<Func<FavoritesItem, bool>> predicate, CancellationToken cancellationToken)
+        {
+            return await dbSet.Where(predicate)
+                              .ToListAsync(cancellationToken);
+        }
+
+        public override async Task RemoveAsync(Guid Id, CancellationToken cancellationToken)
+        {
+            var cartItem = await dbSet.FirstOrDefaultAsync(x => x.Id == Id, cancellationToken)
+                           ?? throw new NotFoundException(Id.ToString(), typeof(FavoritesItem));
+            dbSet.Remove(cartItem);
+        }
+    }
+}
