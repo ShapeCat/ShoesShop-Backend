7222a3fec4f79754ec1794b68162301f2c0580e7
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-07 23:33:32 +0300

    Add queries for adress entity

---
+++ src/Shop.Application/Requests/Queries/GetAdressQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using ShoesShop.Application.Exceptions;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Application.Requests.Base;
+using ShoesShop.Application.Requests.Queries.OutputVMs;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Queries
+{
+    public record GetAdressQuery : IRequest<AdressVm>
+    {
+        public Guid AdressId { get; set; }
+    }
+
+    public class GetAdressQueryHandler : AbstractQueryHandler, IRequestHandler<GetAdressQuery, AdressVm>
+    {
+        public GetAdressQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+        public async Task<AdressVm> Handle(GetAdressQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var adressRepository = unitOfWork.GetRepositoryOf<Adress>();
+                var adress = await adressRepository.FindAllAsync(x => x.Id == request.AdressId, cancellationToken);
+                return mapper.Map<AdressVm>(adress.First());
+            }
+            catch (NotFoundException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ src/Shop.Application/Requests/Queries/GetAllAdressesQuery.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Application.Requests.Base;
+using ShoesShop.Application.Requests.Queries.OutputVMs;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Queries
+{
+    public record GetAllAdressesQuery : IRequest<IEnumerable<AdressVm>> { }
+
+    public class GetAllAdressesQueryHander : AbstractQueryHandler, IRequestHandler<GetAllAdressesQuery, IEnumerable<AdressVm>>
+    {
+        public GetAllAdressesQueryHander(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+        public async Task<IEnumerable<AdressVm>> Handle(GetAllAdressesQuery request, CancellationToken cancellationToken)
+        {
+            var adressRepository = unitOfWork.GetRepositoryOf<Adress>();
+            var adresses = await adressRepository.GetAllAsync(cancellationToken);
+            return mapper.Map<IEnumerable<AdressVm>>(adresses);
+        }
+    }
+}
