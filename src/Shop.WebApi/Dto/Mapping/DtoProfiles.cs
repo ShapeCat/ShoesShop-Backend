e9eaa932b89b564dbef57edfd33cb9c43337d4ba
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-11 20:10:54 +0300

    Remove old Dto mapping profiles

--- src/Shop.WebApi/Dto/Mapping/DtoProfiles.cs
+++ src/Shop.WebApi/Dto/Mapping/DtoProfiles.cs
@@ -18,27 +18,6 @@ namespace ShoesShop.WebApi.Dto.Profiles
                                                        .ForMember(x => x.Street, y => y.MapFrom(x => x.Street))
                                                        .ForMember(x => x.House, y => y.MapFrom(x => x.House))
                                                        .ForMember(x => x.Room, y => y.MapFrom(x => x.Room));
-
-            //CreateMap<ShoesDto, CreateShoesCommand>()
-            //    .ForMember(x => x.Name, y => y.MapFrom(x => x.Name));
-
-            //CreateMap<ShoesDto, UpdateShoesCommand>()
-            //    .ForMember(x => x.Name, y => y.MapFrom(x => x.Name));
-
-            //CreateMap<DescriptionDto, CreateDescriptionCommand>()
-            //    .ForMember(x => x.ReleaseDate, y => y.MapFrom(x => x.ReleaseDate))
-            //    .ForMember(x => x.SkuID, y => y.MapFrom(x => x.SkuID))
-            //    .ForMember(x => x.ColorName, y => y.MapFrom(x => x.ColorName));
-
-            //CreateMap<DescriptionDto, UpdateDescriptionCommand>()
-            //    .ForMember(x => x.ReleaseDate, y => y.MapFrom(x => x.ReleaseDate))
-            //    .ForMember(x => x.SkuID, y => y.MapFrom(x => x.SkuID))
-            //    .ForMember(x => x.ColorName, y => y.MapFrom(x => x.ColorName));
-
-            //CreateMap<ShoesSizeDto, CreateShoesSizeCommand>()
-            //    .ForMember(x => x.Size, y => y.MapFrom(x => x.Size))
-            //    .ForMember(x => x.Price, y => y.MapFrom(x => x.Price))
-            //    .ForMember(x => x.ItemsLeft, y => y.MapFrom(x => x.ItemsLeft));
         }
-}
+    }
 }
