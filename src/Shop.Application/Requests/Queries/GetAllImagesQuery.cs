861310f7645c9f3717db2c03d4ab509bd80633f4
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-11 21:48:46 +0300

    Add return all images request

---
+++ src/Shop.Application/Requests/Queries/GetAllImagesQuery.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Application.Requests.Base;
+using ShoesShop.Application.Requests.Queries.OutputVMs;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Queries
+{
+    public record GetAllImagesQuery : IRequest<IEnumerable<ImageVm>> { }
+
+    public class GetAllImagesQueryHadler : AbstractQueryHandler<GetAllImagesQuery, IEnumerable<ImageVm>>
+    {
+        public GetAllImagesQueryHadler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+        public override async Task<IEnumerable<ImageVm>> Handle(GetAllImagesQuery request, CancellationToken cancellationToken)
+        {
+            var imageRepository = unitOfWork.GetRepositoryOf<Image>(true);
+            var Images = await imageRepository.GetAllAsync(cancellationToken);
+            return mapper.Map<IEnumerable<ImageVm>>(Images);
+        }
+    }
+}
