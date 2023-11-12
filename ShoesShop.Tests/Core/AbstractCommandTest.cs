b2e03f2fe8c5d115565f4afed2455b4b80e9f4b0
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-12 19:07:43 +0300

    Update core for tests

---
+++ ShoesShop.Tests/Core/AbstractCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using ShoesShop.Application.Interfaces;
+using ShoesShop.Persistence;
+
+namespace ShoesShop.Tests.Core
+{
+    public abstract class AbstractCommandTest : IDisposable
+    {
+        protected readonly ShopDbContext DbContext;
+        protected readonly IUnitOfWork UnitOfWork;
+
+        public AbstractCommandTest()
+        {
+            DbContext = ShoesShopTestContext.Create();
+            UnitOfWork = new UnitOfWork(DbContext, false);
+        }
+
+        public void Dispose() => ShoesShopTestContext.Destroy(DbContext);
+    }
+}
---
+++ ShoesShop.Tests/Core/AbstractQueryTest.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Persistence;
+using Xunit;
+
+namespace ShoesShop.Tests.Core
+{
+    [Collection("QueryCollection")]
+    public class AbstractQueryTest
+    {
+        protected readonly IUnitOfWork unitOfWork;
+        protected readonly IMapper mapper;
+
+        public AbstractQueryTest(QueryFixture fixture)
+        {
+            unitOfWork = new UnitOfWork(fixture.DbContext, false);
+            mapper = fixture.Mapper;
+        }
+    }
+}
