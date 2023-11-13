2c3be4e74c1b9109490c0689eccae76f6c919c0b
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-13 22:15:35 +0300

    Add get all models request

---
+++ src/Shop.Application/Requests/Queries/GetAllModelsQuery.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Application.Requests.Base;
+using ShoesShop.Application.Requests.Queries.OutputVMs;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Queries
+{
+    public record GetAllModelsQuery : IRequest<IEnumerable<ModelVm>> { }
+
+    public class GetAllModelsQueryHandler : AbstractQueryHandler<GetAllModelsQuery, IEnumerable<ModelVm>>
+    {
+        public GetAllModelsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+        public override async Task<IEnumerable<ModelVm>> Handle(GetAllModelsQuery request, CancellationToken cancellationToken)
+        {
+            var modelRepository = unitOfWork.GetRepositoryOf<Model>();
+            var allModels = await modelRepository.GetAllAsync(cancellationToken);
+            return mapper.Map<IEnumerable<ModelVm>>(allModels);
+        }
+    }
+}
