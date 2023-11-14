fcda2864d3d7258dced6a41ec0f670e703aba8b7
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-15 02:01:46 +0300

    Add delete model variant request

---
+++ src/Shop.Application/Requests/Commands/DeleteModelVariantCommand.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using ShoesShop.Application.Exceptions;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Application.Requests.Base;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Commands
+{
+    public record DeleteModelVariantCommand : IRequest<Unit>
+    {
+        public Guid ModelvariantId { get; set; }
+    }
+
+    public class DeleteModelVariantCommandHandler : AbstractCommandHandler<DeleteModelVariantCommand, Unit>
+    {
+        public DeleteModelVariantCommandHandler(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        public override async Task<Unit> Handle(DeleteModelVariantCommand request, CancellationToken cancellationToken)
+        {
+            try
+
+            {
+                var modelVariantRepository = unitOfWork.GetRepositoryOf<ModelVariant>();
+                await modelVariantRepository.RemoveAsync(request.ModelvariantId, cancellationToken);
+                await unitOfWork.SaveChangesAsync(cancellationToken);
+                return Unit.Value;
+            }
+            catch (NotFoundException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- src/Shop.WebApi/Controllers/ModelVariantController.cs
+++ src/Shop.WebApi/Controllers/ModelVariantController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ShoesShop.Application.Exceptions;
 using ShoesShop.Application.Requests.Commands;
 using ShoesShop.WebApi.Dto;
 using ShoesShop.WebAPI.Controllers;
@@ -22,6 +23,29 @@ namespace ShoesShop.WebApi.Controllers
             var command = mapper.Map<CreateModelVariantCommand>(modelVariantDto);
             var result = await Mediator.Send(command);
             return Ok(result);
+        }
+
+        [HttpDelete("{modelVariantId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Delete(Guid modelVariantId)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            try
+            {
+                var command = new DeleteModelVariantCommand()
+                {
+                    ModelvariantId = modelVariantId
+                };
+                await Mediator.Send(command);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }
