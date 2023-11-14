cc20fff83985be00c63261f66be76ecb46d5720d
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-14 22:28:13 +0300

    Add get all model sizes request

---
+++ ShoesShop.Tests/Tests/Queries/GetAllModelSizesQueryTests.cs
@@ -0,0 +1,28 @@
+﻿using ShoesShop.Application.Requests.Queries;
+using ShoesShop.Application.Requests.Queries.OutputVMs;
+using ShoesShop.Tests.Core;
+using Shouldly;
+using Xunit;
+
+namespace ShoesShop.Tests.Tests.Queries
+{
+    public class GetAllModelSizesQueryTests : AbstractQueryTests
+    {
+        public GetAllModelSizesQueryTests(QueryFixture fixture) : base(fixture) { }
+
+        [Fact]
+        public async void Should_ReturnAllSizes()
+        {
+            // Arraange
+            var query = new GetAllModelSizesQuery();
+            var handler = new GetAllModelSizesQueryHandler(UnitOfWork, Mapper);
+
+            // Act
+            var allAdresses = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            allAdresses.ShouldAllBe(x => x is ModelSizeVm);
+            allAdresses.Count().ShouldBe(2);
+        }
+    }
+}
---
+++ src/Shop.Application/Requests/Queries/GetAllModelSizesQuery.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using ShoesShop.Application.Interfaces;
+using ShoesShop.Application.Requests.Base;
+using ShoesShop.Application.Requests.Queries.OutputVMs;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Queries
+{
+    public record GetAllModelSizesQuery : IRequest<IEnumerable<ModelSizeVm>> { }
+
+    public class GetAllModelSizesQueryHandler : AbstractQueryHandler<GetAllModelSizesQuery, IEnumerable<ModelSizeVm>>
+    {
+        public GetAllModelSizesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+        public async override Task<IEnumerable<ModelSizeVm>> Handle(GetAllModelSizesQuery request, CancellationToken cancellationToken)
+        {
+            var modelSizeRepository = unitOfWork.GetRepositoryOf<ModelSize>();
+            var allSizes = await modelSizeRepository.GetAllAsync(cancellationToken);
+            return mapper.Map<IEnumerable<ModelSizeVm>>(allSizes);
+        }
+    }
+}
