745b8e3aede7588d14541325491ff552e6464500
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-25 02:41:06 +0300

    Fixed nullable types for phone and address

--- src/Shoes.Entities/User.cs
+++ src/Shoes.Entities/User.cs
@@ -3,16 +3,16 @@
     public class User
     {
         public Guid UserId { get; set; }
-        public Guid AddressId { get; set; }
+        public Guid? AddressId { get; set; }
         public string UserName { get; set; }
         public string Login { get; set; }
         public byte[] Password { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
-        public Address Address { get; set; }
-        public FavoritesList Favorites { get; set; }
-        public ICollection<ShopCart> ShopCarts { get; set; }
-        public ICollection<Order> Orders { get; set; }
-        public ICollection<Review> Reviews { get; set; }
+        public Address? Address { get; set; }
+        public FavoritesList Favorites { get; set; } = new FavoritesList();
+        public ICollection<ShopCart> ShopCarts { get; set; } = new List<ShopCart>();
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
     }
 }
--- src/Shop.Persistance/DbInitializer.cs
+++ src/Shop.Persistance/DbInitializer.cs
@@ -1,10 +1,24 @@
-﻿namespace ShoesShop.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoesShop.Persistence
 {
     public class DbInitializer
     {
         public static void Initialize(ShopDbContext dbContext)
         {
             dbContext.Database.EnsureCreated();
+            FixUserNullableFields(dbContext);
+        }
+
+
+        public static void FixUserNullableFields(ShopDbContext dbContext)
+        {
+            dbContext.Database.ExecuteSqlRaw(@"
+            ALTER TABLE [dbo].[users]
+            ALTER COLUMN [Phone] NVARCHAR(50) NULL;
+            ALTER TABLE [dbo].[users]
+            ALTER COLUMN [AddressId] UNIQUEIDENTIFIER NULL;
+            ");
         }
     }
 }
