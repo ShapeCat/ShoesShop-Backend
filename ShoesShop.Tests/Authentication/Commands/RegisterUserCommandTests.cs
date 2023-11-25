9d04b0c006997850da3893dda5005daf93b1ddb7
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-25 22:17:24 +0300

    Add user register command

---
+++ ShoesShop.Tests/Authentication/Commands/RegisterUserCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using ShoesShop.Application.Common.Exceptions;
+using ShoesShop.Application.Requests.Authentication.Commands;
+using ShoesShop.Entities;
+using ShoesShop.Tests.Core;
+using Shouldly;
+using Xunit;
+
+namespace ShoesShop.Tests.Authentication.Commands
+{
+    public class RegisterUserCommandTests : AbstractCommandTests
+    {
+        [Fact]
+        public async void Should_RegisterNewUser_WhenCorrect()
+        {
+            var command = new RegisterUserCommand()
+            {
+                Login = "register test login",
+                Password = "register test password"
+            };
+            var handler = new RegisterUserCommandHandler(UnitOfWork);
+
+            await handler.Handle(command, CancellationToken.None);
+            DbContext.Users.SingleOrDefault(x => x.Login == command.Login
+                                                 && x.Password == User.HashPassword(command.Password)).ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async void Should_ThrowException_WhenUserWithSameLoginExists()
+        {
+            var command = new RegisterUserCommand()
+            {
+                Login = TestData.ExistedLogin,
+                Password = "register test password"
+            };
+            var handler = new RegisterUserCommandHandler(UnitOfWork);
+
+            await Should.ThrowAsync<AlreadyExistsException>(async () => await handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ src/Shop.Application/Requests/Authentication/Commands/RegisterUserCommand.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using MediatR;
+using ShoesShop.Application.Common.Exceptions;
+using ShoesShop.Application.Common.Interfaces;
+using ShoesShop.Application.Requests.Abstraction;
+using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Authentication.Commands
+{
+    public record RegisterUserCommand : IRequest<Unit>
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(x => x.Login).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+        }
+    }
+
+    public class RegisterUserCommandHandler : AbstractCommandHandler<RegisterUserCommand, Unit>
+    {
+        public RegisterUserCommandHandler(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        public override async Task<Unit> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var userRepository = UnitOfWork.GetRepositoryOf<User>();
+                var sameUsers = await userRepository.FindAllAsync(x => x.Login == request.Login, cancellationToken);
+                if (sameUsers.Any()) throw new AlreadyExistsException(request.Login, typeof(User));
+                var user = new User()
+                {
+                    UserId = Guid.NewGuid(),
+                    UserName = User.GenerateNewUsername(),
+                    Login = request.Login,
+                    Password = User.HashPassword(request.Password),
+                };
+                await userRepository.AddAsync(user, cancellationToken);
+                await UnitOfWork.SaveChangesAsync(cancellationToken);
+                return Unit.Value;
+            }
+            catch (AlreadyExistsException) { throw; }
+        }
+    }
+}
