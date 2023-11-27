0a13e26ab5c4c3ab1645ee5e347b6400c0253aaf
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-27 21:59:44 +0300

    Add roles for user Vm

--- src/Shop.Application/Requests/Users/OutputVMs/UserVm.cs
+++ src/Shop.Application/Requests/Users/OutputVMs/UserVm.cs
@@ -1,7 +1,10 @@
-﻿namespace ShoesShop.Application.Requests.Users.OutputVMs
+﻿using ShoesShop.Entities;
+
+namespace ShoesShop.Application.Requests.Users.OutputVMs
 {
     public class UserVm
     {
+    public Roles Role {  get; set; }
         public Guid UserId { get; set; }
         public string UserName { get; set; }
         public Guid? AddressId { get; set; }
--- src/Shop.Application/Requests/Users/OutputVMs/UserVmProfiles.cs
+++ src/Shop.Application/Requests/Users/OutputVMs/UserVmProfiles.cs
@@ -10,6 +10,7 @@ namespace ShoesShop.Application.Requests.Users.OutputVMs
             CreateMap<User, UserVm>().ForMember(x => x.UserId, y => y.MapFrom(x => x.UserId))
                                      .ForMember(x => x.UserName, y => y.MapFrom(x => x.UserName))
                                      .ForMember(x => x.AddressId, y => y.MapFrom(x => x.AddressId))
+                                     .ForMember(x => x.Role, y => y.MapFrom(x => x.Role))
                                      .ForMember(x => x.Phone, y => y.MapFrom(x => x.Phone));
         }
     }
