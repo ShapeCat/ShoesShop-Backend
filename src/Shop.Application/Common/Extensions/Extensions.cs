60a67d9e4334ab455f8a18a200fb1da564507933
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-11-27 23:51:20 +0300

    Added extensions for repository

---
+++ src/Shop.Application/Common/Extensions/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using ShoesShop.Application.Common.Exceptions;
+using ShoesShop.Application.Common.Interfaces;
+
+namespace ShoesShop.Application.Common.Extensions
+{
+    internal static class Extensions
+    {
+        public static async Task<bool> IsExistsAsync<T>(this IRepositoryOf<T> repository, Guid Id, CancellationToken cancellationToken) where T : class
+        {
+            try
+            {
+                await repository.GetAsync(Id, cancellationToken);
+            }
+            catch (NotFoundException) { return false; }
+            return true;
+        }
+
+        public static async Task<T> FindFirstAsync<T>(this IRepositoryOf<T> repository, Expression<Func<T, bool>> predicate, CancellationToken cancellationToken) where T : class
+        {
+            var entities = await repository.FindAllAsync(predicate, cancellationToken);
+            return entities.First();
+        }
+    }
+}
