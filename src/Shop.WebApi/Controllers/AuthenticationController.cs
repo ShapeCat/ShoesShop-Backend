23db86d2cb2032c396a9b6965963e767f39ca59b
Moonlight_Cat <Moonlight_Cat@SHAPE-NOTEBOOK>
2023-12-06 21:38:00 +0300

    Add authentication endpoints documentation

--- src/Shop.WebApi/Controllers/AuthenticationController.cs
+++ src/Shop.WebApi/Controllers/AuthenticationController.cs
@@ -3,7 +3,6 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ShoesShop.Application.Requests.Authentication.Commands;
-using ShoesShop.Application.Requests.Authentication.OutputVMs;
 using ShoesShop.Application.Requests.Authentication.Queries;
 using ShoesShop.WebApi.Authentication;
 using ShoesShop.WebApi.Dto;
@@ -11,14 +10,39 @@ using ShoesShop.WebAPI.Controllers;
 
 namespace ShoesShop.WebApi.Controllers
 {
+    /// <summary>
+    /// Login/Register user 
+    /// </summary>
     public class AuthenticationController : AbstractController
     {
         private readonly ITokenService tokenService;
 
         public AuthenticationController(IMapper mapper, ITokenService tokenService) : base(mapper) => this.tokenService = tokenService;
 
+        /// <summary>
+        /// Register new user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     Post /api/Authentication/
+        ///     {
+        ///         "login": "login1",
+        ///         "password": "qwerty"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="user">User login data</param>
+        /// <response code="201">Successful Operation</response>
+        /// <response code="409">User with this login already exists</response>
+        /// <response code="400">Validation Error. Check given data</response>
+        /// <response code="500">Server Error. Please, report administrator</response>
         [HttpPost("register")]
         [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> Register([Required] RegisterDto user)
         {
             var command = new RegisterUserCommand()
@@ -32,11 +56,13 @@ namespace ShoesShop.WebApi.Controllers
 
         [HttpPost("login")]
         [AllowAnonymous]
-        public async Task<ActionResult> Login([Required] LoginDto loginDto)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> Login([Required] LoginDto loginDto)
         {
-            AuthenticatedDataVm user;
             var command = Mapper.Map<CheckUserPasswordQuery>(loginDto);
-            user = await Mediator.Send(command);
+            var user = await Mediator.Send(command);
             var token = tokenService.BuildToken(user);
             return Ok(token);
         }
